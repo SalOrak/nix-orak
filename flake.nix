9cbaa224a6106ba79bb8e3ce94b62faf7e28a401
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-12-12 20:28:46 +0100

    setting up the repo as a flake template

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Salorak templates for daily use";
+
+  outputs = {self, ...}: {
+    templates = {
+      rust-toolchain = {
+        path = ./devshells/rust-toolchain;
+        description = "Rust Oxalica's devshell"
+      };
+    };
+  };
+}
