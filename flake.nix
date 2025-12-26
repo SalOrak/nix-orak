933677fcc67e9005a7653abb55dd9f620f55af3e
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-12-26 18:19:39 +0100

    fix: rust template paths

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   outputs = {self, ...}: {
     templates = {
-      rust-toolchain = {
-        path = ./devshells/rust-toolchain;
+      rust = {
+        path = ./devshells/rust;
         description = "Rust Oxalica's devshell";
+      };
+      rust-embedded = {
+        path = ./devshells/rust-embedded/;
+        description = "Embedded Rust Oxalica's devshell";
       };
     };
   };
